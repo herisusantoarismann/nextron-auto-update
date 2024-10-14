69e60cbe8eaf12260dd690337d4854227d04727d
Heri Susanto <heri.susanto@kineticastudios.com>
2024-10-14 10:22:34 +0700

    update: v1.0.7

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -33,7 +33,7 @@ export default function HomePage() {
         <p>
           ⚡ Electron + Next.js ⚡ -<Link href="/next">Go to next page</Link>
         </p>
-        <p>Version v1.0.6</p>
+        <p>Version v1.0.7</p>
         <Image
           src="/images/logo.png"
           alt="Logo image"
